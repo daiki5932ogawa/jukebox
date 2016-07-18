dbfba49d178679bbcbe3fa65b03f57340dc5ed60
Daiki Ogawa <daiki.ogawa@shelfy.jp>
2016-07-18 19:07:57 +0900

    1.Videoエンティティの作成done

---
+++ src/JukeboxBundle/Entity/Video.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace JukeboxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Video
+ *
+ * @ORM\Table(name="video")
+ * @ORM\Entity(repositoryClass="JukeboxBundle\Repository\VideoRepository")
+ */
+class Video
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="owner", type="string", length=255)
+     */
+    private $owner;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_date_played", type="datetime", nullable=true)
+     */
+    private $lastDatePlayed;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Video
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set owner
+     *
+     * @param string $owner
+     *
+     * @return Video
+     */
+    public function setOwner($owner)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * Get owner
+     *
+     * @return string
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Set lastDatePlayed
+     *
+     * @param \DateTime $lastDatePlayed
+     *
+     * @return Video
+     */
+    public function setLastDatePlayed($lastDatePlayed)
+    {
+        $this->lastDatePlayed = $lastDatePlayed;
+
+        return $this;
+    }
+
+    /**
+     * Get lastDatePlayed
+     *
+     * @return \DateTime
+     */
+    public function getLastDatePlayed()
+    {
+        return $this->lastDatePlayed;
+    }
+}
+
